26fa6ffc83e01ddee604e2ca1556b50395807010
bryjonez <91334170+bryjonez@users.noreply.github.com>
2026-05-26 21:12:47 -0700

    adding embassy

---
+++ embassy/embassy.js
@@ -0,0 +1,43 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "embassy_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 90,
+	        "minpan": 0,
+	        "mintilt": -39.01777793050687
+	    },
+	"id": "embassy",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"embassy_tiles/embassy_t_1b_%0y_%0x.jpg"},"down":{"src":"embassy_tiles/embassy_t_1d_%0y_%0x.jpg"},"front":{"src":"embassy_tiles/embassy_t_1f_%0y_%0x.jpg"},"height":16079,"left":{"src":"embassy_tiles/embassy_t_1l_%0y_%0x.jpg"},"right":{"src":"embassy_tiles/embassy_t_1r_%0y_%0x.jpg"},"tilesize":503,"up":{"src":"embassy_tiles/embassy_t_1u_%0y_%0x.jpg"},"width":16079},{"back":{"src":"embassy_tiles/embassy_t_2b_%0y_%0x.jpg"},"down":{"src":"embassy_tiles/embassy_t_2d_%0y_%0x.jpg"},"front":{"src":"embassy_tiles/embassy_t_2f_%0y_%0x.jpg"},"height":8040,"left":{"src":"embassy_tiles/embassy_t_2l_%0y_%0x.jpg"},"right":{"src":"embassy_tiles/embassy_t_2r_%0y_%0x.jpg"},"tilesize":503,"up":{"src":"embassy_tiles/embassy_t_2u_%0y_%0x.jpg"},"width":8040},{"back":{"src":"embassy_tiles/embassy_t_3b_%0y_%0x.jpg"},"down":{"src":"embassy_tiles/embassy_t_3d_%0y_%0x.jpg"},"front":{"src":"embassy_tiles/embassy_t_3f_%0y_%0x.jpg"},"height":4020,"left":{"src":"embassy_tiles/embassy_t_3l_%0y_%0x.jpg"},"right":{"src":"embassy_tiles/embassy_t_3r_%0y_%0x.jpg"},"tilesize":503,"up":{"src":"embassy_tiles/embassy_t_3u_%0y_%0x.jpg"},"width":4020},{"back":{"src":"embassy_tiles/embassy_t_4b_%0y_%0x.jpg"},"down":{"src":"embassy_tiles/embassy_t_4d_%0y_%0x.jpg"},"front":{"src":"embassy_tiles/embassy_t_4f_%0y_%0x.jpg"},"height":2010,"left":{"src":"embassy_tiles/embassy_t_4l_%0y_%0x.jpg"},"right":{"src":"embassy_tiles/embassy_t_4r_%0y_%0x.jpg"},"tilesize":503,"up":{"src":"embassy_tiles/embassy_t_4u_%0y_%0x.jpg"},"width":2010},{"back":{"src":"embassy_tiles/embassy_t_5b_%0y_%0x.jpg"},"down":{"src":"embassy_tiles/embassy_t_5d_%0y_%0x.jpg"},"front":{"src":"embassy_tiles/embassy_t_5f_%0y_%0x.jpg"},"height":1005,"left":{"src":"embassy_tiles/embassy_t_5l_%0y_%0x.jpg"},"right":{"src":"embassy_tiles/embassy_t_5r_%0y_%0x.jpg"},"tilesize":503,"up":{"src":"embassy_tiles/embassy_t_5u_%0y_%0x.jpg"},"width":1005},{"back":{"src":"embassy_tiles/embassy_t_6b_%0y_%0x.jpg"},"down":{"src":"embassy_tiles/embassy_t_6d_%0y_%0x.jpg"},"front":{"src":"embassy_tiles/embassy_t_6f_%0y_%0x.jpg"},"height":503,"left":{"src":"embassy_tiles/embassy_t_6l_%0y_%0x.jpg"},"right":{"src":"embassy_tiles/embassy_t_6r_%0y_%0x.jpg"},"tilesize":503,"up":{"src":"embassy_tiles/embassy_t_6u_%0y_%0x.jpg"},"width":503}],"multilevel":true,"preview":{"src":"embassy_tiles/embassy_preview.jpg"},"projection":"cubic"},
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
